d42781541496c915dcb003da48b4f313ade5b783
abaz1d <abaz.my.id@gmail.com>
2025-04-27 18:42:23 +0700

    Add form for control parameters and AJAX submission to dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
   res.render("dashboard/dashboard", { title: "Polines" });
 });
 
+// POST
+router.post("/", function (req, res, next) {
+  console.log("INI YANG POST DARI DEPAN", req.body);
+});
 module.exports = router;
