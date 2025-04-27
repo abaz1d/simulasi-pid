2b723b88cc05cfb5ef723c127b77d2ff75a28705
abaz1d <abaz.my.id@gmail.com>
2025-04-27 19:34:16 +0700

    Refactor index route to handle user session management and redirect logic

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,56 @@ var router = express.Router();
 
 module.exports = function (db) {
   router.get("/", function (req, res, next) {
-    res.render("index", { title: "Polines" });
+    db.query("SELECT * FROM users", (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.send(err);
+      }
+
+      if (data.rows.length > 0) {
+        // If users table is not empty, set up req.session.user and redirect to /dashboard
+        req.session.user = data.rows[0];
+        return res.redirect("/dashboard");
+      }
+
+      // If users table is empty, render the index page
+      res.render("index", { title: "Polines" });
+    });
+  });
+
+  router.post("/", function (req, res) {
+    const { nama_pengguna } = req.body;
+    db.query(
+      "SELECT * FROM users WHERE nama_pengguna = $1",
+      [nama_pengguna],
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          return res.send(err);
+        }
+
+        if (data.rows.length > 0) {
+          // User already exists
+          req.session.user = data.rows[0];
+          return res.redirect("/dashboard");
+        }
+
+        // User does not exist, insert into database
+        db.query(
+          "INSERT INTO users (nama_pengguna) VALUES ($1) RETURNING *",
+          [nama_pengguna],
+          (err, result) => {
+            if (err) {
+              console.log(err);
+              return res.send(err);
+            }
+
+            req.session.user = result.rows[0];
+            res.redirect("/dashboard");
+          }
+        );
+      }
+    );
   });
 
   return router;
